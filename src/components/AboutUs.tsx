838eb620cd594114bc565b4dfdaed188496c458e
Henias Roses <heniasroses@gmail.com>
2026-06-07 20:03:29 +0800

    Update About Us Page
    > About Us Page is now responsive

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -1,10 +1,19 @@
 export default function AboutUs() {
   return (
     <div className="aboutUs-container">
+
+      {/* MOBILE IMAGE (ONLY SHOWS ON PHONE) */}
+      <div className="aboutUs-mobileImage">
+        <img src="/img/AboutUsBG.png" alt="Henia’s Roses Team" />
+      </div>
+
       <div className="aboutUs-content">
         <h1 className="aboutUs-title">HENIA’S ROSES</h1>
-        <p className="aboutUs-text">Henia’s Roses, established in 2025, is a student game development team under the Far Eastern University – Institute of Technology (FEU Tech), composed of Information Technology students specializing in Animation and Game Development. The group is composed of Leonabel P. Alcantara, Andrie C. Detera, Angel Janica C. Fabregas, and Heniarose Crishia V. Guansing, united by a shared passion for developing games that merge compelling aesthetics with immersive, educational storytelling.</p>
+        <p className="aboutUs-text">
+          Henia’s Roses, established in 2025, is a student game development team under the Far Eastern University – Institute of Technology (FEU Tech), composed of Information Technology students specializing in Animation and Game Development. The group is composed of Leonabel P. Alcantara, Andrie C. Detera, Angel Janica C. Fabregas, and Heniarose Crishia V. Guansing, united by a shared passion for developing games that merge compelling aesthetics with immersive, educational storytelling.
+        </p>
       </div>
+
     </div>
   );
 }
